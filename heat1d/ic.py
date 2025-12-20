b9f1c042de169a1722ce49ebb2c3cb4356973d6f
jhell1717 <jhellewell1722@gmail.com>
2025-12-20 11:29:18 +0000

    updated initial conditions to 2D

--- heat1d/ic.py
+++ heat1d/ic.py
@@ -1,4 +1,8 @@
 import numpy as np
 
-def gaussian_hotspot(x:np.ndarray,base:float,amp:float,x0:float,sigma:float) -> np.ndarray:
-    return base + amp * np.exp(-0.5 * ((x - x0) / sigma) **2)
+def gaussian_hotspot(x:np.ndarray,y:np.ndarray,base:float,amp:float,x0:float,y0:float,sigma:float) -> np.ndarray:
+    X,Y = np.meshgrid(x,y,indexing='xy')
+
+    return base + amp * np.exp(
+        -0.5 * ((x - x0)**2 + (y-y0)**2) / sigma**2
+        )
